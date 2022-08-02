e88d583a14e849bf74bf0ed18f6b637b8c58f0fe
heucedo <hector.ucedo@itti.digital>
2022-08-02 19:33:47 -0400

    9. Modo observador - Whatch me

---
+++ app.js
@@ -0,0 +1,9 @@
+var msg = 'Hola Mundo';
+//msg = 123; //Type 'number' is not assignable to type 'string'
+var hero = {
+    name: 'Ironman',
+    age: 45
+};
+hero.age = 50;
+console.log(hero);
+console.log(hero.age);
---
+++ app.ts
@@ -0,0 +1,14 @@
+
+let msg = 'Hola Mundo';
+
+//msg = 123; //Type 'number' is not assignable to type 'string'
+
+const hero = {
+    name: 'Ironman',
+    age: 45
+}
+
+hero.age = 50
+
+console.log(hero);
+console.log(hero.age);
