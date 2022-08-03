57601cd0bdad5dd659fae1c5fb74418caa110695
heucedo <hector.ucedo@itti.digital>
2022-08-03 19:47:57 -0400

    15. Numbers

--- tipos/booleans.js
+++ tipos/booleans.js
@@ -5,6 +5,9 @@
     let isBatman = false;
     //isSuperman = undefined;
     //console.log({ isBatman }); //Variable 'isSuperman' is used before being assigned
-    isSuperman = true && false;
+    //isSuperman = true && false
+    // Ternario
+    //isSuperman = ( isBatman ) ? true : 'false'; //Type 'string' is not assignable to type 'boolean'.
+    isSuperman = (isBatman) ? true : false;
     console.log({ isSuperman });
 })();
--- tipos/booleans.ts
+++ tipos/booleans.ts
@@ -7,7 +7,11 @@
     //isSuperman = undefined;
     //console.log({ isBatman }); //Variable 'isSuperman' is used before being assigned
     
-    isSuperman = true && false
+    //isSuperman = true && false
+    
+    // Ternario
+    //isSuperman = ( isBatman ) ? true : 'false'; //Type 'string' is not assignable to type 'boolean'.
+    isSuperman = ( isBatman ) ? true : false; 
     
     console.log({ isSuperman }); 
 
