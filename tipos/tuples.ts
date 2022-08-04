baff3fa1da4862501ea93873d47cfcb6724444da
heucedo <hector.ucedo@itti.digital>
2022-08-03 21:28:54 -0400

    19. Tuplas

---
+++ tipos/tuples.ts
@@ -0,0 +1,18 @@
+(()=>{
+
+    /**
+     * En las Tuplas se puede controlar el tipo de datos
+     * en JS la tupla es considerado solo como un Array
+     */
+    const hero: [string, number, boolean] = ['Dr Strange', 100, true];
+
+    //hero[0] = 50; // Type 'number' is not assignable to type 'string'
+    //hero[1] = 'Iron man'; // Type 'string' is not assignable to type 'number'.
+    //hero[2] = 2; //Type 'number' is not assignable to type 'boolean'.
+    hero[0] = 'Iron man'; 
+    hero[1] = 50; 
+    hero[2] = false;
+
+    console.log( hero );
+
+})()
