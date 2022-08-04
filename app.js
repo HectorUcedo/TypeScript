bb7ba21a518404cefb02b668308a79a5c2c2f797
heucedo <hector.ucedo@itti.digital>
2022-08-03 23:15:33 -0400

    24. Ejercicio sin resolver

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 "use strict";
-// Función anónima autoinvocada
 (() => {
-    var a = 10;
-    console.log(a);
+    // Tipos
+    const batman = 'Bruce';
+    const superman = 'Clark';
+    const existe = false;
+    // Tuplas
+    const parejaHeroes = [batman, superman];
+    const villano = ['Lex Lutor', 5, true];
+    // Arreglos
+    const aliados = ['Mujer Maravilla', 'Acuaman', 'San', 'Flash'];
+    //Enumeraciones
+    const fuerzaFlash = 5;
+    const fuerzaSuperman = 100;
+    const fuerzaBatman = 1;
+    const fuerzaAcuaman = 0;
+    // Retorno de funciones
+    function activar_batiseñal() {
+        return 'activada';
+    }
+    function pedir_ayuda() {
+        console.log('Auxilio!!!');
+    }
+    // Aserciones de Tipo
+    const poder = '100';
+    const largoDelPoder = poder.length;
+    console.log(largoDelPoder);
 })();
-//let b: string;
-// b = 3.1415;
-// b = {}; // b podría ser un objeto
-// b = []; // b podría ser un arreglo
-// b = true; // b podría ser un boolean
-// function sayHello( msg: string ){
-//     console.log( msg + ' ' + 'Héctor' );
-// }
