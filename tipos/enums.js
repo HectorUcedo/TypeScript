59c4cfa5c14d66814a62e1cb43911c2c88bca9c0
heucedo <hector.ucedo@itti.digital>
2022-08-03 22:39:53 -0400

    20. Enum

---
+++ tipos/enums.js
@@ -0,0 +1,15 @@
+"use strict";
+(() => {
+    // uppercamel case
+    let AudioLevel;
+    (function (AudioLevel) {
+        AudioLevel[AudioLevel["min"] = 1] = "min";
+        AudioLevel[AudioLevel["medium"] = 2] = "medium";
+        AudioLevel[AudioLevel["medium2"] = 3] = "medium2";
+        AudioLevel[AudioLevel["max"] = 10] = "max";
+    })(AudioLevel || (AudioLevel = {}));
+    // Utilizar siempre los valores parametrizados
+    let currentAudio = AudioLevel.max; //let currentAudio: AudioLevel
+    console.log(currentAudio);
+    //console.log( AudioLevel);
+})();
---
+++ tipos/enums.ts
@@ -0,0 +1,17 @@
+(()=>{
+
+    // uppercamel case
+    enum AudioLevel {
+        min     = 1,
+        medium , // 2
+        medium2, // 3
+        max     = 10
+    } 
+
+    // Utilizar siempre los valores parametrizados
+    let currentAudio = AudioLevel.max; //let currentAudio: AudioLevel
+
+    console.log( currentAudio);
+    //console.log( AudioLevel);
+
+})()
