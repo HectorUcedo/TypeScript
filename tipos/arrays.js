00f5855c4edb19029585716b71e058e0ec677043
heucedo <hector.ucedo@itti.digital>
2022-08-03 21:22:04 -0400

    18. Arreglos

---
+++ tipos/arrays.js
@@ -0,0 +1,15 @@
+"use strict";
+(() => {
+    //const numbers = [1,2,3,4,5,6,7,8,9,10]; // [] arreglos de números
+    //const numbers = [1,2,3,4,5,'6',7,8,9,10]; //const number: (string | number)[]
+    //const numbers: (string | number | boolean )[] = [1,2,3,4,5,'6',7,8,9,10]; // (String o number o buleano)
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const villians = ['Omega Rojo', 'Dormammu', 'Duende Verde'];
+    //numbers.push(true); //Argument of type 'boolean' is not assignable to parameter of type 'string | number'.
+    //numbers.push(11); 
+    //numbers.push(true); 
+    //console.log(numbers);
+    // call back function
+    //numbers.forEach( v => console.log( v.toUpperCase() ) ); // error
+    villians.forEach(v => console.log(v.toUpperCase())); //(method) Array<string>.forEach(callbackfn: (value: string, index: number, array: string[]) => void, thisArg?: any): void
+})();
