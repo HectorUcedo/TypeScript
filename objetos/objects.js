95fe0d5a937db061cb8f7d34240f891818b3bff4
heucedo <hector.ucedo@itti.digital>
2022-08-04 22:46:46 -0400

    34. Cómo crear objetos con tipos específicos

--- objetos/objects.js
+++ objetos/objects.js
@@ -1,1 +1,21 @@
 "use strict";
+(() => {
+    /**
+     * No confundir con un objeto de JS
+     */
+    let flash = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['Super velocidad', 'Viajar en el tiempo']
+    };
+    flash = {
+        //name2 = 'Otro nombre' // Type '{ name2: string; }' is not assignable to type '{ name: "Barry Allen"; age: 24; powers: ["Super velocidad", "Viajar en el tiempo"]; }'.
+        name: 'Clark Kent',
+        //age: 60,
+        powers: ['Súper fuerza'],
+        // getNombre(){
+        //     return this.name;
+        // }
+    };
+    console.log(flash);
+})();
--- objetos/objects.ts
+++ objetos/objects.ts
@@ -4,21 +4,25 @@
      * No confundir con un objeto de JS
      */
 
-    let flash: {
+    let flash: { name: string, age?: number, powers: string[] } = {
         name: 'Barry Allen',
         age: 24,
         powers: ['Super velocidad', 'Viajar en el tiempo']
     }
 
-    // flash = {
-    //     //name2 = 'Otro nombre' // Type '{ name2: string; }' is not assignable to type '{ name: "Barry Allen"; age: 24; powers: ["Super velocidad", "Viajar en el tiempo"]; }'.
-    //     name: 'Clark Kent',
-    //     age: 60,
-    //     powers: ['Súper fuerza'],
-    //     getNombre(){
-    //         return this.name;
-    //     }
-    //}
+    flash = {
+        //name2 = 'Otro nombre' // Type '{ name2: string; }' is not assignable to type '{ name: "Barry Allen"; age: 24; powers: ["Super velocidad", "Viajar en el tiempo"]; }'.
+        name: 'Clark Kent',
+        //age: 60,
+        powers: ['Súper fuerza'],
+        // getNombre(){
+        //     return this.name;
+        // }
+    }
+
+
+
+    console.log(flash);
 
 
 })()
