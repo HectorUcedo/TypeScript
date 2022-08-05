705013452560667762b4f28829be37e362a3558a
heucedo <hector.ucedo@itti.digital>
2022-08-04 22:33:46 -0400

    35. Objetos básicos

---
+++ objetos/objects.ts
@@ -0,0 +1,24 @@
+(()=> {
+
+    /**
+     * No confundir con un objeto de JS
+     */
+
+    let flash: {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['Super velocidad', 'Viajar en el tiempo']
+    }
+
+    // flash = {
+    //     //name2 = 'Otro nombre' // Type '{ name2: string; }' is not assignable to type '{ name: "Barry Allen"; age: 24; powers: ["Super velocidad", "Viajar en el tiempo"]; }'.
+    //     name: 'Clark Kent',
+    //     age: 60,
+    //     powers: ['Súper fuerza'],
+    //     getNombre(){
+    //         return this.name;
+    //     }
+    //}
+
+
+})()
