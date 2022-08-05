a068c8efd24921a335fee894332a31d05de39c72
heucedo <hector.ucedo@itti.digital>
2022-08-04 20:31:09 -0400

    33. tarea y Resoución del ejercicio práctico #2

--- funciones/function-type.js
+++ funciones/function-type.js
@@ -4,16 +4,16 @@
     const greet = (name) => `Hola ${name}`;
     const saveTheWorld = () => `El mundo está salvado`;
     //*let myFunction;
-        ** let;
-    myFunction: (y, z) => number;
+    //*let myFunction: (y: number, z: number) => number;
+    //*let myFunction: (y: string) => string;
     let myFunction;
     //!myFunction = 10; //Type 'number' is not assignable to type 'Function'
     //*console.log(myFunction);
     // Estoy pasando por referencia
-    myFunction = addNumber; //Type '(a: number, b: number) => number' is not assignable to type '() => number'.
-    console.log(myFunction(1, 2));
-    myFunction = greet;
-    console.log(greet('Héctor'));
+    // myFunction = addNumber; //Type '(a: number, b: number) => number' is not assignable to type '() => number'.
+    // console.log(myFunction(1,2));
+    //? myFunction = greet;
+    //? console.log(greet('Héctor'));
     myFunction = saveTheWorld;
     console.log(saveTheWorld());
 })();
--- funciones/function-type.ts
+++ funciones/function-type.ts
@@ -5,18 +5,19 @@
     const saveTheWorld = () => `El mundo está salvado`;
 
     //*let myFunction;
-    **let myFunction: (y: number, z: number) => number;
-    let myFunction: (y: string) => string;
+    //*let myFunction: (y: number, z: number) => number;
+    //*let myFunction: (y: string) => string;
+    let myFunction: () => string;
 
     //!myFunction = 10; //Type 'number' is not assignable to type 'Function'
     //*console.log(myFunction);
     
     // Estoy pasando por referencia
-    myFunction = addNumber; //Type '(a: number, b: number) => number' is not assignable to type '() => number'.
-    console.log(myFunction(1,2));
+    // myFunction = addNumber; //Type '(a: number, b: number) => number' is not assignable to type '() => number'.
+    // console.log(myFunction(1,2));
 
-    myFunction = greet;
-    console.log(greet('Héctor'));
+    //? myFunction = greet;
+    //? console.log(greet('Héctor'));
 
     myFunction = saveTheWorld;
     console.log(saveTheWorld());
